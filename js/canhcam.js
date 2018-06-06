8946388fae9557525e29a4d44db67783b1fae0b9
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-06 15:39:29 +0700

    commit

--- js/canhcam.js
+++ js/canhcam.js
@@ -114,35 +114,35 @@ function b64DecodeUnicode(str) {
 }
 
 
-// var FULL_URL = 'http://kienlongbanklucky.canhcam.gb/api/'
-// var AppURL = {
-// 	giai_dacbiet: FULL_URL + 'AwardService.aspx?method=data_giaidacbiet',
-// 	giai_dacbiet_update: FULL_URL + 'AwardService.aspx?p=',
-// 	giai_khuyenkhich: FULL_URL + 'AwardService.aspx?method=data_giaikhuyenkhich',
-// 	giai_khuyenkhich_update: FULL_URL + 'AwardService.aspx?p=',
-// 	giai_nhat: FULL_URL + 'AwardService.aspx?method=data_giainhat',
-// 	giai_nhat_update: FULL_URL + 'AwardService.aspx?p=',
-// 	giai_nhi: FULL_URL + 'AwardService.aspx?method=data_giainhi',
-// 	giai_nhi_update: FULL_URL + 'AwardService.aspx?p=',
-// 	kiemtra: FULL_URL + "AwardService.aspx?method=",
-// 	main: FULL_URL + "AwardService.aspx?method=Control",
-// 	index: "./json/index.json",
-// 	results: FULL_URL + "AwardService.aspx?method=data_results"
-// };
+var FULL_URL = 'http://preview6455.canhcam.com.vn/api/'
 var AppURL = {
-	giai_dacbiet: './json/data_giaidacbiet.json',
-	giai_dacbiet_update: '/?p=',
-	giai_khuyenkhich: './json/data_giaikhuyenkhich.json',
-	giai_khuyenkhich_update: '/?p=',
-	giai_nhat: './json/data_giainhat.json',
-	giai_nhat_update: '/?p=',
-	giai_nhi: './json/data_giainhi.json',
-	giai_nhi_update: '/?p=',
-	kiemtra: "./json/data_searchresult.json",
-	main: "./json/control.json",
+	giai_dacbiet: FULL_URL + 'AwardService.aspx?method=data_giaidacbiet',
+	giai_dacbiet_update: FULL_URL + 'AwardService.aspx?p=',
+	giai_khuyenkhich: FULL_URL + 'AwardService.aspx?method=data_giaikhuyenkhich',
+	giai_khuyenkhich_update: FULL_URL + 'AwardService.aspx?p=',
+	giai_nhat: FULL_URL + 'AwardService.aspx?method=data_giainhat',
+	giai_nhat_update: FULL_URL + 'AwardService.aspx?p=',
+	giai_nhi: FULL_URL + 'AwardService.aspx?method=data_giainhi',
+	giai_nhi_update: FULL_URL + 'AwardService.aspx?p=',
+	kiemtra: FULL_URL + "AwardService.aspx?method=",
+	main: FULL_URL + "AwardService.aspx?method=Control",
 	index: "./json/index.json",
-	results: "./json/data_results.json"
+	results: FULL_URL + "AwardService.aspx?method=data_results"
 };
+// var AppURL = {
+// 	giai_dacbiet: './json/data_giaidacbiet.json',
+// 	giai_dacbiet_update: '/?p=',
+// 	giai_khuyenkhich: './json/data_giaikhuyenkhich.json',
+// 	giai_khuyenkhich_update: '/?p=',
+// 	giai_nhat: './json/data_giainhat.json',
+// 	giai_nhat_update: '/?p=',
+// 	giai_nhi: './json/data_giainhi.json',
+// 	giai_nhi_update: '/?p=',
+// 	kiemtra: "./json/data_searchresult.json",
+// 	main: "./json/control.json",
+// 	index: "./json/index.json",
+// 	results: "./json/data_results.json"
+// };
 
 function getUrlVars() {
     var vars = [], hash;

750a138efca70f9c4b628cf40d796cc199dd201e
Phaj Nướng <baonguyenyam@gmail.com>
2018-08-06 14:53:47 +0700

    fixed

--- js/creative.js
+++ js/creative.js
@@ -1,5 +1,5 @@
-// var FULL_URL = '/api/'
-var FULL_URL = 'http://preview6455.canhcam.com.vn/api/'
+var FULL_URL = '/api/'
+// var FULL_URL = 'http://preview6455.canhcam.com.vn/api/'
 var AppURL = {
 	giai_dacbiet: FULL_URL + 'AwardService.aspx?method=data_',
 	giai_dacbiet_update: FULL_URL + 'AwardService.aspx?p=',
